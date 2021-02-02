b0cf3e9d7ece188eef4ae76d893bd2a1feb0e854
ldizon8 <lawdizon8@gmail.com>
2021-02-01 17:38:15 -0800

    Initial commit

---
+++ JustWatchIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustWatchIt
+//
+//  Created by Lawrence Dizon on 2/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
