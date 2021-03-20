436d21d055e699077908351d3336a829ae4b2bec
ldizon8 <lawdizon8@gmail.com>
2021-03-19 22:22:06 -0700

    Add UISearchBar to SearchVC

--- JustWatchIt/View Model/SearchViewController.swift
+++ JustWatchIt/View Model/SearchViewController.swift
@@ -7,10 +7,18 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
-
+class SearchViewController: UIViewController, UISearchBarDelegate {
+    var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        searchBar = UISearchBar()
+        searchBar.frame = CGRect(x: 0, y:50, width: view.bounds.size.width, height: 70)
+        searchBar.delegate = self
+        searchBar.searchBarStyle = UISearchBar.Style.default
+        searchBar.placeholder = "Search a movie"
+        view.addSubview(searchBar)
         print("Search VC loaded")
     }
 
--- JustWatchIt/View Model/TabBarViewController.swift
+++ JustWatchIt/View Model/TabBarViewController.swift
@@ -7,7 +7,7 @@ class TabBarViewController: UITabBarController {
         super.viewDidLoad()
 
         let homeVC = HomeViewController()
-        homeVC.tabBarItem = UITabBarItem(tabBarSystemItem: .history, tag: 0)
+        homeVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "paperplane.fill"), tag: 0)
         
         let searchVC = SearchViewController()
         searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 1)
