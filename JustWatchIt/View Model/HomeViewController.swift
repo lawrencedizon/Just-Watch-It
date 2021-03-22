623c1c2c45eb79dc883acdb26ca330bc625b816a
ldizon8 <lawdizon8@gmail.com>
2021-03-21 18:33:20 -0700

    Create titles for HomeVC

--- JustWatchIt/View Model/HomeViewController.swift
+++ JustWatchIt/View Model/HomeViewController.swift
@@ -13,7 +13,25 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         
         print("HomeVC loaded")
+        view.backgroundColor = .white
+        
+        /* Brute-force approach, use AutoLayout later*/
+        let title1 = UITextView(frame: CGRect(x: 0, y: 50, width: 300, height: 100))
+        let title2 = UITextView(frame: CGRect(x: 0, y: 100, width: 300, height: 100))
+        let title3 = UITextView(frame: CGRect(x: 0, y: 150, width: 300, height: 100))
+        let title4 = UITextView(frame: CGRect(x: 0, y: 200, width: 300, height: 100))
+        
+        title1.text = "Popular"
+        title2.text = "Now in Theaters"
+        title3.text = "Upcoming Movies"
+        title4.text = "Top Rated Movies"
+        view.addSubview(title1)
+        view.addSubview(title2)
+        view.addSubview(title3)
+        view.addSubview(title4)
+        
     }
+    
     
 
     
