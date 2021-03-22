6639539ad033ed6971046a66183f8f7a773174e3
ldizon8 <lawdizon8@gmail.com>
2021-03-21 19:03:17 -0700

    Create HomeVC's basic layout

--- JustWatchIt/View Model/HomeViewController.swift
+++ JustWatchIt/View Model/HomeViewController.swift
@@ -15,16 +15,20 @@ class HomeViewController: UIViewController {
         print("HomeVC loaded")
         view.backgroundColor = .white
         
-        /* Brute-force approach, use AutoLayout later*/
-        let title1 = UITextView(frame: CGRect(x: 0, y: 50, width: 300, height: 100))
-        let title2 = UITextView(frame: CGRect(x: 0, y: 100, width: 300, height: 100))
-        let title3 = UITextView(frame: CGRect(x: 0, y: 150, width: 300, height: 100))
-        let title4 = UITextView(frame: CGRect(x: 0, y: 200, width: 300, height: 100))
+        /* Brute-force approach, use AutoLayout later */
+        let title1 = UILabel(frame: CGRect(x: 10, y: 60, width: 300, height: 100))
+        let title2 = UILabel(frame: CGRect(x: 10, y: 260, width: 300, height: 100))
+        let title3 = UILabel(frame: CGRect(x: 10, y: 460, width: 300, height: 100))
+        let title4 = UILabel(frame: CGRect(x: 10, y: 660, width: 300, height: 100))
         
         title1.text = "Popular"
+        title1.font = title1.font.withSize(20)
         title2.text = "Now in Theaters"
+        title2.font = title2.font.withSize(20)
         title3.text = "Upcoming Movies"
+        title3.font = title3.font.withSize(20)
         title4.text = "Top Rated Movies"
+        title4.font = title4.font.withSize(20)
         view.addSubview(title1)
         view.addSubview(title2)
         view.addSubview(title3)
