20bcb3ab58052589714963f70567d95b398e3bda
ldizon8 <lawdizon8@gmail.com>
2021-03-23 18:40:22 -0700

    Add NSLayoutConstraints to CollectionViews and UILabels

--- JustWatchIt/View Model/HomeViewController.swift
+++ JustWatchIt/View Model/HomeViewController.swift
@@ -13,6 +13,38 @@ class HomeViewController: UIViewController {
     weak var collectionViewB: UICollectionView!
     weak var collectionViewC: UICollectionView!
     weak var collectionViewD: UICollectionView!
+    
+    private let title1: UILabel = {
+        let title1 = UILabel()
+        title1.translatesAutoresizingMaskIntoConstraints = false
+        title1.text = "Popular"
+        title1.font = title1.font.withSize(20)
+        return title1
+    }()
+    
+    private let title2: UILabel = {
+        let title2 = UILabel()
+        title2.translatesAutoresizingMaskIntoConstraints = false
+        title2.text = "In Theaters"
+        title2.font = title2.font.withSize(20)
+        return title2
+    }()
+    
+    private let title3: UILabel = {
+        let title3 = UILabel()
+        title3.translatesAutoresizingMaskIntoConstraints = false
+        title3.text = "Coming Soon"
+        title3.font = title3.font.withSize(20)
+        return title3
+    }()
+    
+    private let title4: UILabel = {
+        let title4 = UILabel()
+        title4.translatesAutoresizingMaskIntoConstraints = false
+        title4.text = "Top Rated"
+        title4.font = title4.font.withSize(20)
+        return title4
+    }()
 
     
     var data: [Int] = Array(0..<100)
@@ -22,24 +54,31 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         print("HomeVC loaded")
         view.backgroundColor = .white
+        
+        view.addSubview(title1)
+        view.addSubview(title2)
+        view.addSubview(title3)
+        view.addSubview(title4)
+        addConstraints()
+       
                
         self.collectionViewA.register(MovieCollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCellA")
         self.collectionViewB.register(MovieCollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCellB")
         self.collectionViewC.register(MovieCollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCellC")
         self.collectionViewD.register(MovieCollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCellD")
-        
+
         self.collectionViewA.alwaysBounceVertical = true
         self.collectionViewA.backgroundColor = .white
-        
+
         self.collectionViewB.alwaysBounceVertical = true
         self.collectionViewB.backgroundColor = .white
-        
+
         self.collectionViewC.alwaysBounceVertical = true
         self.collectionViewC.backgroundColor = .white
-        
+
         self.collectionViewD.alwaysBounceVertical = true
         self.collectionViewD.backgroundColor = .white
-        
+
         self.collectionViewA.delegate = self
         self.collectionViewB.delegate = self
         self.collectionViewC.delegate = self
@@ -50,93 +89,100 @@ class HomeViewController: UIViewController {
         self.collectionViewC.dataSource = self
         self.collectionViewD.dataSource = self
         
-        //
-        
-        /* Brute-force approach, use AutoLayout later */
-                let title1 = UILabel(frame: CGRect(x: 10, y: 60, width: 300, height: 100))
-                let title2 = UILabel(frame: CGRect(x: 10, y: 260, width: 300, height: 100))
-                let title3 = UILabel(frame: CGRect(x: 10, y: 460, width: 300, height: 100))
-                let title4 = UILabel(frame: CGRect(x: 10, y: 660, width: 300,height: 100))
-                
-                title1.translatesAutoresizingMaskIntoConstraints = false
-                title2.translatesAutoresizingMaskIntoConstraints = false
-                title3.translatesAutoresizingMaskIntoConstraints = false
-                title4.translatesAutoresizingMaskIntoConstraints = false
-                
-        
-
-
-        
-                title1.text = "Popular"
-                title1.font = title1.font.withSize(20)
-                title2.text = "Now in Theaters"
-                title2.font = title2.font.withSize(20)
-                title3.text = "Upcoming Movies"
-                title3.font = title3.font.withSize(20)
-                title4.text = "Top Rated Movies"
-                title4.font = title4.font.withSize(20)
-//                view.addSubview(title1)
-//                view.addSubview(title2)
-//                view.addSubview(title3)
-//                view.addSubview(title4)
         
     }
     
     override func loadView() {
         super.loadView()
-        
+
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
-        
+
         let collectionViewA = UICollectionView(frame: .zero, collectionViewLayout:layout)
         collectionViewA.translatesAutoresizingMaskIntoConstraints = false
         collectionViewA.collectionViewLayout = layout
-        
+
         let collectionViewB = UICollectionView(frame: .zero, collectionViewLayout:layout)
         collectionViewB.translatesAutoresizingMaskIntoConstraints = false
         collectionViewB.collectionViewLayout = layout
-        
+
         let collectionViewC = UICollectionView(frame: .zero, collectionViewLayout:layout)
         collectionViewC.translatesAutoresizingMaskIntoConstraints = false
         collectionViewC.collectionViewLayout = layout
-        
+
         let collectionViewD = UICollectionView(frame: .zero, collectionViewLayout:layout)
         collectionViewD.translatesAutoresizingMaskIntoConstraints = false
         collectionViewD.collectionViewLayout = layout
-        
+
         self.collectionViewA = collectionViewA
         self.collectionViewB = collectionViewB
         self.collectionViewC = collectionViewC
         self.collectionViewD = collectionViewD
-        
+
         self.view.addSubview(collectionViewA)
         self.view.addSubview(collectionViewB)
         self.view.addSubview(collectionViewC)
         self.view.addSubview(collectionViewD)
+    }
+    
+    private func addConstraints(){
+        var constraints = [NSLayoutConstraint]()
+        
+        //Add constraints
+        
+        //Title1
+        constraints.append(title1.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20))
+        constraints.append(title1.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10))
+        constraints.append(title1.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor))
+       
+        //Title2
+        constraints.append(title2.topAnchor.constraint(equalTo: collectionViewA.bottomAnchor, constant: 10))
+        constraints.append(title2.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10))
+        constraints.append(title2.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor))
         
         
-        //Use NSLayoutAnchors in the future
+        //Title3
+        constraints.append(title3.topAnchor.constraint(equalTo: collectionViewB.bottomAnchor, constant: 10))
+        constraints.append(title3.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10))
+        constraints.append(title3.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor))
         
-        collectionViewA.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
-                collectionViewA.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
-                collectionViewA.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
-                collectionViewA.heightAnchor.constraint(equalToConstant: view.frame.width/2).isActive = true
         
-        collectionViewB.topAnchor.constraint(equalTo: view.topAnchor, constant: 240).isActive = true
-                collectionViewB.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
-                collectionViewB.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
-                collectionViewB.heightAnchor.constraint(equalToConstant: view.frame.width/2).isActive = true
+        //Title4
+        constraints.append(title4.topAnchor.constraint(equalTo: collectionViewC.bottomAnchor, constant: 10))
+        constraints.append(title4.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10))
+        constraints.append(title4.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor))
         
-        collectionViewC.topAnchor.constraint(equalTo: view.topAnchor, constant: 440).isActive = true
-                collectionViewC.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
-                collectionViewC.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
-                collectionViewC.heightAnchor.constraint(equalToConstant: view.frame.width/2).isActive = true
         
-        collectionViewD.topAnchor.constraint(equalTo: view.topAnchor, constant: 640).isActive = true
-                collectionViewD.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
-                collectionViewD.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
-                collectionViewD.heightAnchor.constraint(equalToConstant: view.frame.width/2).isActive = true
         
+        // CollectionViewA
+        constraints.append(collectionViewA.topAnchor.constraint(equalTo: title1.topAnchor, constant: 30))
+        constraints.append(collectionViewA.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10))
+        constraints.append(collectionViewA.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor))
+        
+        constraints.append(collectionViewA.heightAnchor.constraint(equalToConstant: view.frame.width/2))
+
+        // CollectionViewB
+        constraints.append(collectionViewB.topAnchor.constraint(equalTo: title2.bottomAnchor, constant: 10))
+        constraints.append(collectionViewB.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10))
+        constraints.append(collectionViewB.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor))
+        constraints.append(collectionViewB.heightAnchor.constraint(equalToConstant: view.frame.width/2))
+        
+        // CollectionViewC
+        constraints.append(collectionViewC.topAnchor.constraint(equalTo: title3.topAnchor, constant: 30))
+        constraints.append(collectionViewC.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10))
+        constraints.append(collectionViewC.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor))
+        constraints.append(collectionViewC.heightAnchor.constraint(equalToConstant: view.frame.width/2))
+
+        // CollectionViewD
+        constraints.append(collectionViewD.topAnchor.constraint(equalTo: title4.topAnchor, constant: 30))
+        constraints.append(collectionViewD.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10))
+        constraints.append(collectionViewD.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor))
+        constraints.append(collectionViewD.heightAnchor.constraint(equalToConstant: view.frame.width/2))
+        
+         
+               
+        //Activate (applying)
+               NSLayoutConstraint.activate(constraints)
     }
 }
 
@@ -146,6 +192,8 @@ extension HomeViewController: UICollectionViewDataSource{
         data.count
     }
    
+    
+    // == is a heavy operation use tags instead
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if collectionView == collectionViewA {
